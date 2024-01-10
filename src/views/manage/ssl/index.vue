98d5f495f91f75e6a14d9e96d0bdfb36f7e04bfc
buyfakett <buyfakett@vip.qq.com>
2024-01-10 16:05:47 +0800

    完成所有界面

--- src/views/manage/ssl/index.vue
+++ src/views/manage/ssl/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="app-container">
     <el-button type="primary" @click="onAdd" style="margin-bottom: 10px">新增</el-button>
+    <el-popconfirm
+      title="确定刷新证书吗？"
+      @confirm="refresh()"
+      confirm-button-text="确认"
+      cancel-button-text="取消"
+      style="margin-left: 10px"
+    >
+      <el-button type="primary" slot="reference" style="margin-bottom: 10px" icon="el-icon-refresh">手动刷新证书</el-button>
+    </el-popconfirm>
     <el-table
       v-loading="listLoading"
       :data="list"
@@ -20,18 +29,17 @@
         </template>
       </el-table-column>
       <el-table-column label="创建时间" align="center">
-        <template slot-scope="{row}"  >
+        <template slot-scope="{row}">
           <a class="link-type" @click="onEdit(row)">{{ row.register_time }}</a>
         </template>
       </el-table-column>
       <el-table-column label="到期时间" align="center">
-        <template slot-scope="{row}"  >
+        <template slot-scope="{row}">
           <a class="link-type" @click="onEdit(row)">{{ row.exp_time }}</a>
         </template>
       </el-table-column>
       <el-table-column label="状态" align="center">
-        <template slot-scope="{row}"  >
-<!--          <a class="link-type" @click="onEdit(row)">{{ getStatusString(row.status) }}</a>-->
+        <template slot-scope="{row}">
           <el-tag :type="getStatusTagType(row.status)">{{ getStatusString(row.status) }}</el-tag>
         </template>
       </el-table-column>
@@ -41,36 +49,51 @@
           <el-popconfirm
             title="确定删除吗？"
             @confirm="handleDelete(row.id)"
-            confirm-button-text='确认'
-            cancel-button-text='取消'
-            style="margin-left: 10px">
+            confirm-button-text="确认"
+            cancel-button-text="取消"
+            style="margin-left: 10px"
+          >
             <el-button type="danger" slot="reference" icon="el-icon-delete">删除</el-button>
           </el-popconfirm>
         </template>
       </el-table-column>
     </el-table>
     <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
-      <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="100px" style="width: 400px; margin-left:50px;">
-        <el-form-item label="主域名" prop="certificate_domain">
+      <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="100px"
+               style="width: 400px; margin-left:50px;"
+      >
+        <el-form-item label="主域名" prop="first_domain_id">
           <el-select
-            v-model="appNameValue"
+            v-model="domainValue"
             filterable
             placeholder="请选择主域名"
-            @change="fetchData(value,appNameValue)"
+            @change="changeDomain(domainValue)"
           >
             <el-option
-              v-for="appNameItem in appNameList"
-              :key="appNameItem.value"
-              :label="appNameItem.label"
-              :value="appNameItem.value"
+              v-for="domain in chooseDomainList"
+              :key="domain.value"
+              :label="domain.label"
+              :value="domain.value"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="服务器" prop="certificate_domain">
+          <el-select
+            v-model="serverValue"
+            multiple
+            placeholder="请选择服务器"
+            @change="changeServer(serverValue)"
+          >
+            <el-option
+              v-for="server in chooseServerList"
+              :key="server.value"
+              :label="server.label"
+              :value="server.value"
             />
           </el-select>
         </el-form-item>
         <el-form-item label="域名" prop="certificate_domain">
-          <el-input v-model="temp.certificate_domain" />
-        </el-form-item>
-        <el-form-item label="状态" prop="status">
-          <el-input v-model="temp.status" />
+          <el-input v-model="temp.certificate_domain"/>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -86,7 +109,9 @@
 </template>
 
 <script>
-import { getList, addSsl, editSsl, deleteSsl } from '@/api/ssl'
+import { addSsl, deleteSsl, editSsl, refreshSsl, sslList } from '@/api/ssl'
+import { domainList } from '@/api/domain'
+import { serverList } from '@/api/server'
 
 export default {
   filters: {
@@ -94,7 +119,7 @@ export default {
       const statusMap = {
         published: 'success',
         draft: 'gray',
-        deleted: 'danger',
+        deleted: 'danger'
       }
       return statusMap[status]
     }
@@ -111,106 +136,151 @@ export default {
       },
       temp: {
         id: undefined,
+        first_domain_id: undefined,
+        server_ids: [],
         certificate_domain: '',
-        register_time: '',
-        exp_time: '',
-        status: '',
       },
+      domainValue: '',
+      serverValue: '',
+      chooseDomainList: [],
+      chooseServerList: [],
+      serverData: [],
+      domainData: []
     }
   },
   created() {
-    this.fetchData()
+    this.getSslList()
   },
   methods: {
-    createData(){
+    refresh(){
+      this.listLoading = true
+      refreshSsl()
+        .then(res => {
+          if (res.code === 0) {
+            this.listLoading = false
+          }
+        })
+    },
+    async getDomainList() {
+      await domainList()
+        .then(res => {
+          if (res.code === 0) {
+            this.domainData = res.data.items
+          }
+        })
+    },
+    async getServerList() {
+      await serverList()
+        .then(res => {
+          if (res.code === 0) {
+            this.serverData = res.data.items
+          }
+        })
+    },
+    getSslList() {
+      this.listLoading = true
+      sslList()
+        .then(res => {
+          if (res.code === 0) {
+            this.list = res.data.items
+            this.listLoading = false
+          }
+        })
+    },
+    createData() {
       addSsl(this.temp)
         .then(res => {
           if (res.code === 0) {
             this.dialogFormVisible = false
-            this.listLoading = true
-            getList().then(response => {
-              this.list = response.data.items
-              this.listLoading = false
-            })
+            this.getSslList()
           }
         })
     },
-    updateData(){
-      console.log(this.temp)
+    changeDomain(first_domain_id) {
+      this.temp.first_domain_id = first_domain_id
+    },
+    changeServer(server_ids) {
+      this.temp.server_ids = server_ids
+    },
+    updateData() {
       editSsl(this.temp)
         .then(res => {
           if (res.code === 0) {
             this.dialogFormVisible = false
-            this.listLoading = true
-            getList().then(response => {
-              this.list = response.data.items
-              this.listLoading = false
-            })
+            this.getSslList()
           }
         })
-    },
-    fetchData() {
-      this.listLoading = true
-      getList().then(response => {
-        this.list = response.data.items
-        this.listLoading = false
-      })
     },
     getStatusString(status) {
       switch (status) {
         case 0:
-          return '不进行续期';
+          return '不进行续期'
         case 1:
-          return '正常';
+          return '正常'
         case 2:
-          return '新建/过期';
+          return '新建/过期'
       }
     },
     getStatusTagType(status) {
       switch (status) {
         case 0:
-          return 'info';
+          return 'info'
         case 1:
-          return 'success';
+          return 'success'
         case 2:
-          return 'warning';
+          return 'warning'
       }
     },
-    onEdit(row) {
-      console.log(row)
+    async onEdit(row) {
+      await this.getFromData()
       this.temp = Object.assign({}, row) // copy obj
       this.dialogFormVisible = true
       this.dialogStatus = 'update'
     },
-    onAdd() {
+    async onAdd() {
+      await this.getFromData()
       this.resetTemp()
       this.dialogFormVisible = true
       this.dialogStatus = 'create'
+    },
+    async getFromData(){
+      await this.getDomainList()
+      await this.getServerList()
+      this.chooseServerList = []
+      this.chooseDomainList = []
+      this.domainValue = undefined
+      this.serverValue = []
+      for (var domain in this.domainData) {
+        this.chooseDomainList.push({
+          value: this.domainData[domain]['id'],
+          label: this.domainData[domain]['domain'],
+        })
+      }
+      for (var server in this.serverData) {
+        this.chooseServerList.push({
+          value: this.serverData[server]['id'],
+          label: this.serverData[server]['hostname']
+        })
+      }
     },
     // 清除数据
     resetTemp() {
       this.temp = {
         id: undefined,
+        first_domain_id: undefined,
+        server_ids: [],
         certificate_domain: '',
-        register_time: '',
-        exp_time: '',
-        status: '',
       }
     },
     handleDelete(server_id) {
-      console.log(server_id)
       deleteSsl(server_id)
         .then(res => {
           if (res.code === 0) {
             this.dialogFormVisible = false
-            this.listLoading = true
-            getList().then(response => {
-              this.list = response.data.items
-              this.listLoading = false
-            })
+            this.getSslList()
           }
         })
-    },
+    }
   }
 }
 </script>
