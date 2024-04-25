5019c612875387061fee6837bd56dd79bd7514e3
buyfakett <buyfakett@vip.qq.com>
2024-04-25 17:21:32 +0800

    fix: 修改打印版本号位置

--- src/permission.js
+++ src/permission.js
@@ -5,7 +5,6 @@ import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
-import { getServerVersion } from '@/api/version'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
@@ -40,19 +39,6 @@ router.beforeEach((to, from, next) => {
       NProgress.done()
     }
   }
-
-  // 获取前端和后端的版本号在控制台打印
-  const version = require('./../package.json').version
-  console.log(`当前前端版本号: V${version}`)
-  getServerVersion()
-    .then(res => {
-      if (res.code === 0) {
-        console.log(`当前服务端版本为: V${res.data.version}`)
-      }
-    })
-    .catch(error => {
-      console.error('获取服务端版本失败:', error)
-    })
 })
 
 router.afterEach(() => {
